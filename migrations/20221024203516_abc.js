645eedefdca2bb685eb19dfa09927c450e96dc5c
Eric JL Richard <eric.jl.richard@gmail.com>
2022-11-07 23:15:26 -0500

    added readme

--- migrations/20221024203516_abc.js
+++ migrations/20221024203516_abc.js
@@ -4,26 +4,7 @@
  */
 exports.up = function(knex) {
   return knex.schema
-    .createTable('users', (table) => {
-      table.increments('id').primary();
-      table.integer('github_id').notNullable();
-      table.string('avatar_url').notNullable();
-      table.string('username').notNullable();
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    })
-    .createTable('posts', (table) => {
-      table.increments('id').primary();
-      table.integer('user_id').unsigned().notNullable();
-      table.string('title', 75).notNullable();
-      table.text('content').notNullable();
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-      table
-        .foreign('user_id')
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-    })
+    
     .createTable("boxer", (table) => {
       table.increments("id").primary();
       table.string("first_name").notNullable();
